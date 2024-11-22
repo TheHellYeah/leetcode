c1c9918e449441dddff7df8dd73c247fa3a3d16c
akostyuchenkov <akostyuchenkov@phoenixit.ru>
2024-11-22 13:30:18 +0300

    1072. Flip Columns For Maximum Number of Equal Rows

---
+++ src/S1072.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class S1072 {
+
+    public int maxEqualRowsAfterFlips(int[][] matrix) {
+        Map<Integer, Integer> occurs = new HashMap<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            occurs.merge(Arrays.hashCode(matrix[i]), 1, Integer::sum);
+            occurs.merge(Arrays.hashCode(reverse(matrix[i])), 1, Integer::sum);
+        }
+        return occurs.values().stream().max(Integer::compareTo).orElse(0);
+    }
+
+    private int[] reverse(int[] arr) {
+        int[] reversed = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            int val = arr[i];
+            reversed[i] = val == 0 ? 1 : 0;
+        }
+        return reversed;
+    }
+}
